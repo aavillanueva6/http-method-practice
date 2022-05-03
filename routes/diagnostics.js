b01f1ea6badbebf89897ec73feda05a081aaf95f
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-02 23:34:58 -0700

    adds GET and POST method handlers

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -1,15 +1,34 @@
 const diagnostics = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const timestamp = require('unix-timestamp');
 
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
-  // TODO: Logic for sending all the content of db/diagnostics.json
+  console.info(`${req.method} request received to diagnostics`);
+  readFromFile('./db/diagnostics.json').then((data) =>
+    res.json(JSON.parse(data))
+  );
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
-  // TODO: Logic for appending data to the db/diagnostics.json file
+  console.info(`${req.method} request received to diagnostics`);
+  const { errors } = req.body;
+
+  const newDiagnostic = {
+    time: timestamp.now(),
+    error_id: uuidv4(),
+    errors,
+  };
+
+  readAndAppend(newDiagnostic, './db/diagnostics.json');
+  const response = {
+    status: 'success',
+    body: newFeedback,
+  };
+
+  res.json(response);
 });
 
 module.exports = diagnostics;
